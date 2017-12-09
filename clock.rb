eb5d2c9325172eba8398f09c5eea0bac95529a17
Art Rodriguez <art.rodriguez@live.com>
2017-12-08 20:26:12 -0500

    add Clockwork gem

---
+++ clock.rb
@@ -0,0 +1,22 @@
+require 'clockwork'
+require './config/boot'
+require './config/environment'
+require 'nokogiri'
+require 'open-uri'
+
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+
+    Port.scrape_port_wait_times("https://apps.cbp.gov/bwt/bwt.xml")
+    puts "Success!"
+  end
+
+  # handler receives the time when job is prepared to run in the 2nd argument
+  # handler do |job, time|
+  #   puts "Running #{job}, at #{time}"
+  # end
+
+  every(30.minutes, 'scraper.job')
+
+end
